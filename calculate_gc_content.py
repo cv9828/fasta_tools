e3cf91598d78a92835d654acda952dd43f5ddf41
Casey Pearring <pearring@hawaii.edu>
2015-09-30 20:40:25 -1000

    did stuff
    did stuff

--- calculate_gc_content.py
+++ calculate_gc_content.py
@@ -8,14 +8,23 @@ a_count = 0
 c_count = 0
 g_count = 0
 t_count = 0
+total_count = 0
 
 with open(sys.argv[1], 'r') as fasta:
-	a_count = str.count("A",  0, len(sys.argv[1]))
 
-#    for line in fasta:
-#        if line.startswith(">"):
-#            # Header line, skip it
-#            continue
+    for line in fasta:
+        if line.startswith(">"):
+            # Header line, skip it
+            continue
+        a_count = line.count("A")
+        c_count = line.count("C")
+        g_count = line.count("G")
+        t_count = line.count("T")
+
+total_count = a_count + c_count + g_count + t_count
+#here is where the word count looks weird
+
+
 #        for base in line.strip():
 #            if base == "A":
 #                a_count += 1
@@ -31,3 +40,10 @@ print("A: %d" % a_count)
 print("C: %d" % c_count)
 print("G: %d" % g_count)
 print("T: %d" % t_count)
+#now for percentages
+print("Percentages: ")
+print("A: %d" % a_count / total_count)
+print("C: %d" % c_count / total_count)
+print("G: %d" % g_count / total_count)
+print("T: %d" % t_count / total_count)
+print("CG: %d" % (c_count + g_count) / total_count)
